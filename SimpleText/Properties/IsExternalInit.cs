0311f571416bca230ab0e4096c6257581ab1b5bd
兰小方 <lanxiaofang@cvte.com>
2022-01-20 14:55:31 +0800

    编译器设置，支持 init 特性。

---
+++ SimpleText/Properties/IsExternalInit.cs
@@ -0,0 +1,9 @@
+﻿#if !NET5_0 && !NET6_0  
+
+namespace System.Runtime.CompilerServices
+{
+    // 为什么定义这个类型，参见：https://developercommunity.visualstudio.com/t/error-cs0518-predefined-type-systemruntimecompiler/1244809
+    internal static class IsExternalInit { }
+}
+
+#endif
