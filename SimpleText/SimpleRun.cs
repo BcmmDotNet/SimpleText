603867fa4605adde52d70bfdc56fe03747fd7446
兰小方 <lanxiaofang@cvte.com>
2022-01-20 14:56:16 +0800

    添加字符段落。

---
+++ SimpleText/SimpleRun.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace SimpleText
+{
+    internal class SimpleRun
+    {
+        /// <summary>
+        /// 象形字符集合。
+        /// </summary>
+        public Collection<GlyphChar> GlyphChars { get; set; }
+
+        /// <summary>
+        /// 行距。
+        /// </summary>
+        public int LineSpacing { get; set; }
+    }
+}
